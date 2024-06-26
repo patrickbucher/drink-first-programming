472b1120c410f84111f41ce45a280e56e2f81ae4
Patrick Bucher <patrick.bucher@mailbox.org>
2024-06-26 20:50:26 +0200

    horizonal/vertical win condition check

--- session-01/connect_four/src/lib.rs
+++ session-01/connect_four/src/lib.rs
@@ -1,9 +1,17 @@
+const CONNECT_N: usize = 4;
 const WIDTH: usize = 7;
 const HEIGHT: usize = 6;
 
 const EMPTY: char = '_';
-const PLAYER_ONE: char = 'x';
-const PLAYER_TWO: char = 'o';
+pub const PLAYER_ONE: char = 'x';
+pub const PLAYER_TWO: char = 'o';
+
+#[derive(Debug)]
+pub enum Outcome {
+    Win(char),
+    Draw,
+    Nothing { row: usize, col: usize },
+}
 
 pub struct Board {
     fields: Vec<Vec<char>>,
@@ -36,6 +44,61 @@ impl Board {
         }
         output
     }
+
+    pub fn drop_stone(&mut self, stone: char, slot: usize) -> Result<Outcome, String> {
+        if stone != PLAYER_ONE && stone != PLAYER_TWO {
+            return Err(format!("{stone} is not an allowed stone"));
+        }
+        match slot {
+            1..=WIDTH => {
+                let slot_index = slot - 1;
+                for r in (0..HEIGHT).rev() {
+                    let mut row = &mut self.fields[r];
+                    if row[slot_index] == EMPTY {
+                        row[slot_index] = stone;
+                        return Ok(Outcome::Nothing {
+                            row: r,
+                            col: slot_index,
+                        });
+                    }
+                }
+                Err(format!("slot {slot} is already full"))
+            }
+            _ => Err(format!("slot {slot} is out of range [{};{}]", 1, WIDTH)),
+        }
+    }
+
+    fn is_horizontal_win(&self, r: usize, p: char) -> bool {
+        let mut matches = 0;
+        let row = &self.fields[r];
+        println!("{:?}", row);
+        for f in row {
+            if f == &p {
+                matches += 1;
+                if matches == CONNECT_N {
+                    return true;
+                }
+            } else {
+                matches = 0;
+            }
+        }
+        matches == CONNECT_N
+    }
+
+    fn is_vertical_win(&self, c: usize, p: char) -> bool {
+        let mut matches = 0;
+        for row in &self.fields {
+            if row[c] == p {
+                matches += 1;
+                if matches == CONNECT_N {
+                    return true;
+                }
+            } else {
+                matches = 0;
+            }
+        }
+        matches == CONNECT_N
+    }
 }
 
 #[cfg(test)]
@@ -59,5 +122,41 @@ mod tests {
         for row in board.fields {
             assert_eq!(row.len(), WIDTH);
         }
+    }
+
+    #[test]
+    fn test_horizontal_not_win() {
+        let board = Board::new();
+        let actual = board.is_horizontal_win(0, PLAYER_ONE);
+        assert_eq!(actual, false);
+    }
+
+    #[test]
+    fn test_horizontal_win() {
+        let mut board = Board::new();
+        board.drop_stone(PLAYER_ONE, 1);
+        board.drop_stone(PLAYER_ONE, 2);
+        board.drop_stone(PLAYER_ONE, 3);
+        board.drop_stone(PLAYER_ONE, 4);
+        let actual = board.is_horizontal_win(HEIGHT - 1, PLAYER_ONE);
+        assert_eq!(actual, true);
+    }
+
+    #[test]
+    fn test_vertical_not_win() {
+        let board = Board::new();
+        let actual = board.is_vertical_win(0, PLAYER_TWO);
+        assert_eq!(actual, false);
+    }
+
+    #[test]
+    fn test_vertical_win() {
+        let mut board = Board::new();
+        board.drop_stone(PLAYER_TWO, 3);
+        board.drop_stone(PLAYER_TWO, 3);
+        board.drop_stone(PLAYER_TWO, 3);
+        board.drop_stone(PLAYER_TWO, 3);
+        let actual = board.is_vertical_win(2, PLAYER_TWO);
+        assert_eq!(actual, true);
     }
 }
--- session-01/connect_four/src/main.rs
+++ session-01/connect_four/src/main.rs
@@ -1,6 +1,13 @@
-use connect_four::Board;
+use connect_four::{Board, PLAYER_ONE, PLAYER_TWO};
 
 fn main() {
-    let board = Board::new();
+    let mut board = Board::new();
     println!("{}", board.render());
+    for i in 0..4 {
+        println!("{:?}", board.drop_stone(PLAYER_ONE, 1));
+        println!("{:?}", board.drop_stone(PLAYER_TWO, 1));
+    }
+    println!("{}", board.render());
+    println!("{:?}", board.drop_stone('$', 3));
+    println!("{:?}", board.drop_stone(PLAYER_ONE, 7));
 }
